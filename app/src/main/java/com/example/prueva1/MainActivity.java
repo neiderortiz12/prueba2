136f6cfebad5845cd1f96cd589410ec7b25e623c
Neider <neiderfabi12@hotmail.com>
2019-09-28 00:48:44 -0500

    segunda ver

--- app/src/main/java/com/example/prueva1/MainActivity.java
+++ app/src/main/java/com/example/prueva1/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.prueva1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button bpress;
+    public EditText name;
+    public TextView mostrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bpress = (Button) findViewById(R.id.bpress);
+        name = (EditText) findViewById(R.id.name);
+        mostrar = (TextView) findViewById(R.id.mostrar);
+        bpress.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                mostrar.setText(name.getText());
+            }
+        });
     }
 }
